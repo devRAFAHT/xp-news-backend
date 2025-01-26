58e16e45a666359dc023cf98406a189b085e0c04
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2025-01-26 19:23:46 -0300

    Feat: Full CRUD added to controller

--- src/main/java/br/edu/ifba/xpnewsbackend/user/controller/UserController.java
+++ src/main/java/br/edu/ifba/xpnewsbackend/user/controller/UserController.java
@@ -3,9 +3,7 @@ package br.edu.ifba.xpnewsbackend.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +13,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+    @PostMapping("/create")
+    public ResponseEntity<Void> create(String dto){
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping("/find-all")
     public ResponseEntity<List<String>> findAll(Pageable pageable){
         List<String> usuarios = new ArrayList<>();
         return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping(value = "find-by-id", params = "id")
+    public ResponseEntity<String> findById(@RequestParam ("id") Long id){
+        return ResponseEntity.ok("João");
+    }
+
+    @PutMapping(value = "update", params = "id")
+    public ResponseEntity<Void> update(@RequestParam ("id") Long id){
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "delete", params = "id")
+    public ResponseEntity<Void> delete(@RequestParam ("id") Long id){
+        return ResponseEntity.noContent().build();
     }
 
 }
